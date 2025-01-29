fc90ac33acb5c19d6bea2164068a7599b91f2600
nolduakali <88775244+ariadesupriyatna@users.noreply.github.com>
2025-01-29 23:31:22 +0700

    Update 0x00010.hs

--- Indonesia/Project.Euler/0x00010.hs
+++ Indonesia/Project.Euler/0x00010.hs
@@ -1,12 +1,8 @@
-import EulerLib (listPrimes)
+limit = 2000000 :: Int
+main = putStrLn (show ans)
+ans = sum (filter isPrime [2 .. (limit - 1)])
 
-compute :: IO String
-compute = do
-    let ans = sum (listPrimes 1999999)
-    return (show ans)
-
-main :: IO ()
-main = do
-    result <- compute
-    putStrLn result
-
+isPrime :: Int -> Bool
+isPrime n
+    | n < 2     = False
+    | otherwise = null [ x | x <- [2..(floor . sqrt . fromIntegral) n], n `mod` x == 0]
